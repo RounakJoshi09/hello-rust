1fb55ca0dcb18f7f1b6c3271c37e494648dee11d
RounakBytes <rounakjsh783@gmail.com>
2024-08-16 21:30:28 +0530

    Traits Completed

--- src/bin/traits.rs
+++ src/bin/traits.rs
@@ -1,17 +1,3 @@
-// Topic: Traits
-//
-// Requirements:
-// * Calculate the perimeter of a square and triangle:
-//   * The perimeter of a square is the length of any side*4.
-//   * The perimeter of a triangle is a+b+c where each variable
-//     represents the length of a side.
-// * Print out the perimeter of the shapes
-//
-// Notes:
-// * Use a trait to declare a perimeter calculation function
-// * Use a single function to print out the perimeter of the shapes
-//   * The function must utilize impl trait as a function parameter
-
 trait Mensuration {
     fn perimeter(&self) -> f64;
 }
@@ -48,7 +34,7 @@ fn check_box_fit(shape: impl Mensuration) -> bool {
 }
 
 fn main() {
-    let square = Square { side: 80.3 };
+    let square = Square { side: 40.3 };
     let triangle = Triangle {
         side1: 10.2,
         side2: 20.3,
