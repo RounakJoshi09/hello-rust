8967b1c0d1258f2dcc8e9f7d135e44601edd6592
RounakBytes <rounakjsh783@gmail.com>
2024-08-18 01:38:53 +0530

    Slices Done

---
+++ src/bin/activity36.rs
@@ -0,0 +1,27 @@
+// Topic: Arrays & Slices
+//
+// Requirements:
+// * Print pairs of numbers and their sums as they are streamed from a data source
+// * If only one number is received, then print "Unpaired value: V",
+//   where V is the value
+// * If no numbers are received, print "Data stream complete"
+//
+// Notes:
+// * A simulated data stream is already configured in the code
+// * See the stdlib docs for the "chunks" method on "slice" for more info
+
+fn data() -> &'static [u64] {
+    &[5, 5, 4, 4, 3, 3, 1]
+}
+
+fn main() {
+    // `stream` is an iterator of Option<&[u64]>
+    let mut stream = data().chunks(2);
+
+    stream.for_each(|data| match data {
+        [first, second] => println!("First Number : {:?}, Second Number :{:?}", first, second),
+        [single] => println!("Unpaired Number : {:?}", single),
+        [] => println!("No Number Recieved"),
+        [..] => unreachable!("Improper Number Recieved"),
+    });
+}
